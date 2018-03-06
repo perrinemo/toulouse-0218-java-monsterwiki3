85b148b07ed6bf39a4ed1a0161272d05b11bb10f
Perrine <morandperrine@hotmail.fr>
2018-03-06 18:49:59 +0100

    Modification ScrollAdapter + MonsterActivity + activity_monter

--- app/src/main/java/fr/wildcodeschool/monsterlegends/ScrollAdapter.java
+++ app/src/main/java/fr/wildcodeschool/monsterlegends/ScrollAdapter.java
@@ -1,4 +1,27 @@
 package fr.wildcodeschool.monsterlegends;
 
-public class ScrollAdapter {
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+public class ScrollAdapter extends FragmentPagerAdapter {
+
+    private final List fragments;
+
+    public ScrollAdapter(FragmentManager fm, List fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return (Fragment) this.fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return this.fragments.size();
+    }
 }
