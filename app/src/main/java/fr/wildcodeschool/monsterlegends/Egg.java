6bb0714cf3e33abbbcff961530f0688a5be5ae75
Crock Nys <francoisnys@gmail.com>
2018-03-15 16:51:56 +0100

    easter egg V2

--- app/src/main/java/fr/wildcodeschool/monsterlegends/Egg.java
+++ app/src/main/java/fr/wildcodeschool/monsterlegends/Egg.java
@@ -2,8 +2,11 @@ package fr.wildcodeschool.monsterlegends;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -17,23 +20,30 @@ public class Egg extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_egg);
 
-        final Button egg =  findViewById(R.id.egg);
+        final ImageButton egg =  findViewById(R.id.egg);
         final TextView scoreBoard = findViewById(R.id.score);
 
         egg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                DisplayMetrics displayMetrics = egg.getResources().getDisplayMetrics();
+                float dpHeight = displayMetrics.heightPixels / displayMetrics.density;
+                float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+
+                int y = (int) dpHeight;
+                int x = (int) dpWidth;
+
                 Random r = new Random();
-                int i2 = r.nextInt(2000 - 100);
+                int i2 = r.nextInt( y - (y * (-1)) );
 
                 Random t = new Random();
-                int i1 = t.nextInt(800 - 100);
+                int i1 = t.nextInt(x - (x * (-1)) );
                 egg.setX(i1);
                 egg.setY(i2);
 
                 score = score + 1 ;
-                scoreBoard.setText("score: " + score);
+                scoreBoard.setText("score: " + score );
             }
         });
     }
